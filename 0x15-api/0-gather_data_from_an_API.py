7809e371e4414ad7bef4b41b92b3968afb868439
StephenAnthony8 <anthonysteve02@gmail.com>
2024-02-19 17:42:46 +0300

    q0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+""" API intro practise module """
+import json
+import sys
+from urllib import request, error
+
+
+def main():
+    """executes request_get function"""
+    try:
+        employee_id = int(sys.argv[1])
+        request_get(employee_id)
+    except TypeError as e:
+        pass
+
+
+def request_get(employee_id):
+    """ Uses REST API to return info about an employees TODO list """
+
+    # user url
+    url_user = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+
+    # name of employee
+    with request.urlopen(f"{url_user}") as user:
+        name = json.loads(user.read()).get('name')
+
+    # number of total tasks
+    with request.urlopen(f"{url_user}/todos") as f:
+        total_todo = len(json.loads(f.read()))
+
+    # completed tasks
+    with request.urlopen(f"{url_user}/todos?completed=true") as f:
+        true_complete = json.loads(f.read())
+
+    complete_status = f"{len(true_complete)}/{total_todo}"
+
+    # output
+    print(f"Employee {name} is done with tasks({complete_status})")
+    for task in true_complete:
+        print(f"\t {task.get('title')}")
+
+
+if __name__ == "__main__":
+    main()
