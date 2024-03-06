3f5ae7ae4a4bf2900f283d627a90a8972e3a61ad
StephenAnthony8 <anthonysteve02@gmail.com>
2024-03-06 04:57:50 +0300

    q0 - 3 & readme

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Script querying reddit API """
+import json
+import requests
+import sys
+
+
+def number_of_subscribers(subreddit):
+    """ Queries the number of subscribers a subreddit has """
+
+    subreddit_url = f"https://www.reddit.com/r/{subreddit}.json"
+
+    subs_count = requests.get(subreddit_url)
+
+    if (subs_count.status_code == 200):
+        subs_count = json.loads(subs_count.text)
+        r_dict = subs_count.get('data')
+        r_child = r_dict.get('children')[0]
+        r_data = r_child.get('data')
+        r_sub = r_data.get('subreddit_subscribers')
+
+        return (r_sub)
+    else:
+        return (0)
+
+
+if __name__ == "__main__":
+    print(number_of_subscribers(sys.argv[1]))
---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Script querying reddit API """
+import json
+import requests
+import sys
+
+
+def top_ten(subreddit):
+    """ Queries the first 10 hot posts of a subreddit """
+
+    hot_url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    hot_posts = requests.get(hot_url)
+
+    if (hot_posts.status_code == 200):
+        h_posts = json.loads(hot_posts.text).get('data').get('children')
+
+        for post in h_posts:
+            print(post.get('data').get('title'))
+    else:
+        print(None)
+
+
+if __name__ == "__main__":
+    top_ten(sys.argv[1])
